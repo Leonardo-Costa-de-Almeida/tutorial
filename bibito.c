1de10ca370e04b987bee5e4ca59445a0e5aba4e1
Alyson Matheus <alyson1907@gmail.com>
2020-03-27 00:33:47 -0300

    primeiro commit

---
+++ bibito.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int contador;
+  contador = 0;
+  for (contador = 100; contador >= 20; contador--) {
+    printf("\n %i", contador);
+  }
+  system("PAUSE");
+}
